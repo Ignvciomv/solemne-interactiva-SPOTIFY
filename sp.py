71cff4cd401e1bf28da9da3d97f53421b15a830a
Ignvciomv <gfi.matus.araya@gmail.com>
2025-11-24 16:59:39 -0300

    Add files via upload

--- sp.py
+++ sp.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 # Carga el archivo CSV "" en un DataFrame de pandas.
-df = pd.read_csv("spotify_songs.csv")
+df = pd.read_csv("spotify_songs.csv.zip")
 
 # Muestra un título y una descripción en la aplicación Streamlit.
 st.write("""
